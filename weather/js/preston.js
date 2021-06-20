52c802932d3a1609b9bcd8eb6bea6d87c6ab77b5
hopperjoel <hopper.joel.w@gmail.com>
2021-06-19 23:29:27 -0600

    sdf

--- weather/js/preston.js
+++ weather/js/preston.js
@@ -2,3 +2,6 @@ function toggleMenu() {
     console.log(document.getElementById("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide");
   }
+
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleString();
