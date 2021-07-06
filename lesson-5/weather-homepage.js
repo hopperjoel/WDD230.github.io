aa07b587fe6eb18d0d7bdc61cb37fb71d4d3ae6a
hopperjoel <hopper.joel.w@gmail.com>
2021-07-06 10:28:40 -0600

    homepage

--- lesson-5/weather-homepage.js
+++ lesson-5/weather-homepage.js
@@ -5,26 +5,44 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject)
-        const towns = jsonObject['towns'];
-        for (let i = 0; i < towns.length; i++ ) {
+        const towns = jsonObject["towns"];
 
+        const townpages = towns.filter(
+            (town) =>
+            town.name == "Fish Haven" ||
+            town.name == "Soda Springs" ||
+            town.name == "Preston"
+        );
+
+        townpages.forEach((town) => {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
-            let h3 = document.createElement('h3')
+            let h3 = document.createElement('h3');
+            let figure = document.createElement('figure');
             let img = document.createElement('img');
             let p = document.createElement('p');
-            h2.textContent = towns[i].name;
-            h3.textContent = towns[i].motto;
-            img.setAttribute('src', towns[i].photo);
-            p.innerHTML = 'Year Founded: ' + towns[i].yearFounded + '<br>' +
-            'Population ' + towns[i].currentPopulation + '<br>' +
-            'Average Rainfall ' + towns[i].averageRainfall;
+
+            h2.textContent = `${town.name}`;
+            h3.textContent = `${town.motto}`;
+            p.innerHTML = `Year Founded: ${town.yearFounded}<br> Population: ${town.currentPopulation}<br>
+            Annual Rainfall: ${town.averageRainfall}`;
+
+            card.setAttribute("class", "town-info");
+            figure.setAttribute("class", "town-image");
+            img.setAttribute("src", `${town.photo}`);
+            img.setAttribute("alt", `Town of ${town.name}`);
+
             card.appendChild(h2);
             card.appendChild(h3);
-            card.appendChild(img);
+            card.appendChild(figure);
+            figure.appendChild(img);
             card.appendChild(p);
             
             document.querySelector('div.cards').appendChild(card);
-        }
+        });
     });
+
+function toggleMenu() {
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+    }
