2dfeea4768b1fb85e8bafc864fa7a7c4a6c30c79
hopperjoel <hopper.joel.w@gmail.com>
2021-07-10 13:42:43 -0600

    Big modifications to css and preston api

--- lesson-10/js/preston-weatherapi.js
+++ lesson-10/js/preston-weatherapi.js
@@ -4,15 +4,33 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-        document.getElementById('current-temp').textContent = jsObject.list[0].main.temp;
-        document.getElementById('current-temp').textContent = jsObject.list[0].main.temp;
-        document.getElementById('current-temp').textContent = jsObject.list[0].main.temp;
-        document.getElementById('current-temp').textContent = jsObject.list[0].main.temp;
-        document.getElementById('current-temp').textContent = jsObject.list[0].main.temp;
-        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png';
-        const desc = jsObject.list[0].weather[0].description;
-        document.getElementById('imagesrc').textContent = imagesrc;
-        document.getElementById('icon').setAttribute('src', imagesrc);
-        document.getElementById('icon').setAttribute('alt', desc);
-        
+        document.getElementById('current-condition').innerHTML = jsObject.list[0].weather[0].main;
+        document.getElementById('current-temp').innerHTML = jsObject.list[0].main.temp;
+        document.getElementById('temp-high').innerHTML = jsObject.list[0].main.temp_max;
+        document.getElementById('wind-chill').textContent = jsObject.list[0].main.temp;
+        document.getElementById('humidity').innerHTML = jsObject.list[0].main.humidity;
+        document.getElementById('wind-speed').innerHTML = jsObject.list[0].wind.speed;
+        var j = 1;
+        const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        for(var i = 0; i < jsObject.list.length; i++) {
+            var dayTime = jsObject.list[i].dt_txt;
+            var today = new Date();
+
+            if (dayTime.includes("18:00:00")) {
+                var iconsrc = 'https://openweathermap.org/img/w/' + jsObject.list[i].weather[0].icon + '.png';
+                var imagesrc = jsObject.list[i].weather[0].icon;
+                var imagesrc = imagesrc.slice(-3, -3);
+                var desc = jsObject.list[i].weather[0].description;
+                var temp = jsObject.list[i].main.temp;
+                var dateFormat = new Date(dayTime);
+                document.getElementById(`imagesrc-${j}`).textContent = imagesrc;
+                document.getElementById(`icon-${j}`).setAttribute('src', iconsrc);
+                document.getElementById(`icon-${j}`).setAttribute('alt', desc);
+                document.getElementById(`temp-${j}`).textContent = temp;
+                document.getElementById(`day-${j}`).textContent = weekDays[dateFormat.getDay()];
+                j++;
+            };
+        };
+
     });
