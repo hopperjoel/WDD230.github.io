9534ff1f10c4e1602c4c47bf3c9597c7c0e0b3c1
hopperjoel <hopper.joel.w@gmail.com>
2021-07-10 14:01:39 -0600

    https fix?

--- lesson-10/js/preston-weatherapi.js
+++ lesson-10/js/preston-weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=dd504892e20d342c74bd24c89e54dc9a"
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=dd504892e20d342c74bd24c89e54dc9a"
 
 fetch(apiURL)
     .then((response) => response.json())
