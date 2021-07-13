a114be1ff707114cc2cd9ee1fa5de0d74aa3c91f
hopperjoel <hopper.joel.w@gmail.com>
2021-07-13 13:33:43 -0600

    updating all css and footers

--- lesson-11/js/weather-homepage.js
+++ lesson-11/js/weather-homepage.js
@@ -50,4 +50,7 @@ fetch(requestURL)
 function toggleMenu() {
     console.log(document.getElementById("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide");
-    }
+    };
+
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleString();
