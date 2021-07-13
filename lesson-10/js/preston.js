008d4ae0a0520ed63d893210cb4f170130f7cebd
hopperjoel <hopper.joel.w@gmail.com>
2021-07-12 18:07:50 -0600

    adding announcement

--- lesson-10/js/preston.js
+++ lesson-10/js/preston.js
@@ -5,3 +5,12 @@ function toggleMenu() {
 
 var dt = new Date();
 document.getElementById("datetime").innerHTML = dt.toLocaleString();
+
+function announceDisplay() {
+    var currentDay = new Date()
+    if (currentDay.getDay() == 5) {
+        document.getElementById("friday-announcement").style.display = "block"
+    } else {
+        document.getElementById("friday-announcement").style.display = "none"
+    }
+}
