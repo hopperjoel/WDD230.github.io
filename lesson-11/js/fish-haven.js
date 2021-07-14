9ad04557864596664423ef2a3b4dda38a78a141d
hopperjoel <hopper.joel.w@gmail.com>
2021-07-14 15:14:27 -0600

    sDf

--- lesson-11/js/fish-haven.js
+++ lesson-11/js/fish-haven.js
@@ -1,3 +1,47 @@
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json"
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const towns = jsonObject["towns"];
+
+        const fishHaven = towns.filter(
+            (town) =>
+            town.name == "Fish Haven"
+            );
+
+        fishHaven.forEach((town) => {
+            let card = document.createElement('section');
+            let div = document.createElement('div');
+            let h2 = document.createElement('h2');
+            let h3First = document.createElement('h3');
+            let h3Second = document.createElement('h3');
+            let h3Third = document.createElement('h3');
+            let h3Fourth = document.createElement('h3');
+
+            h2.textContent = 'Upcoming Events:';
+            h3First.textContent = `${town.events[0]}`;
+            h3Second.textContent = `${town.events[1]}`;
+            h3Third.textContent = `${town.events[2]}`;
+            h3Fourth.textContent = `${town.events[3]}`;
+
+            card.setAttribute("class", "events");
+            div.setAttribute("class", "event-title");
+
+            card.appendChild(div1);
+            div.appendChild(h2);
+            card.appendChild(h3First);
+            card.appendChild(h3Second);
+            card.appendChild(h3Third);
+            card.appendChild(h3Fourth);
+            
+            document.querySelector('div.card').appendChild(card);
+        });
+    });
+
+
 function toggleMenu() {
     console.log(document.getElementById("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide");
@@ -11,3 +55,4 @@ if (dt.getDay() == 5) {
 } else {
     document.getElementById("friday-announcement").style.display = "none"
 }
+
