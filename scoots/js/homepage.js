402f669bee421c5e94aea4f0d3b28d7abc8dbac4
hopperjoel <hopper.joel.w@gmail.com>
2021-07-17 13:27:57 -0600

    szegrdsrg]|

--- scoots/js/homepage.js
+++ scoots/js/homepage.js
@@ -1,33 +1,45 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides();
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var slides = document.getElementsByClassName("hero-slide");
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
 function toggleMenu() {
     console.log(document.getElementById("primaryNav").classList);
     document.getElementById("primaryNav").classList.toggle("hide");
-    };
+};
+
+function floatingNav() {
+  const navbar = document.querySelector("nav");
+  const navbarHeight = 100;
+  const distanceFromTop = Math.abs(
+    document.body.getBoundingClientRect().top
+  );
+
+  if (distanceFromTop >= navbarHeight) {navbar.classList.add("fixed-top");}
+  else {navbar.classList.remove("fixed-top");}
+};
+
+document.addEventListener("scroll", floatingNav());
+    
+// var button = document.querySelector("button");
+// noOpacity = false;
+
+// button.addEventListener("click", function opacityChange() {
+//   if(noOpacity){
+//     document.getElementsByTagName("nav").style.backgroundColor = "white";
+//     noOpacity = false;
+//   } else {
+//     document.getElementsByTagName("nav").style.backgroundColor = "rgba(46, 196, 182, 1.0)";
+//     noOpacity = true;
+//   }
+// });
